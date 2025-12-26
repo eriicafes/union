ea98af776ee24bd68a537cbd62b664e8704fbb44
Eric Afes <eriicafes@gmail.com>
2025-12-26 18:35:33 +0100

    Fix changeset publish command

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 NEW_VERSION=$(grep -m 1 '"version"' package.json | awk -F '"' '{print $4}')
 echo "New version is v$NEW_VERSION"
-git tag -a v$NEW_VERSION -m "v$NEW_VERSION"
+git tag -a v$NEW_VERSION -m "Release v$NEW_VERSION" && echo "Created tag v$NEW_VERSION"
